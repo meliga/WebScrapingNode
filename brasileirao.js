c868efb13d701586e0884d9d376145af61b7c79d
diedromeliga <diedromeliga@gmail.com>
2017-07-14 15:45:40 -0300

    coletando manualmente os principais dados da rodada

--- brasileirao.js
+++ brasileirao.js
@@ -5,11 +5,37 @@
  */
 
 var request = require('request');
+var cheerio = require('cheerio');
 
 function cartola(error, response, body) {
     console.log('error:', error);
     console.log('statusCode:', response && response.statusCode);
-    console.log('body', body);
+    //console.log('body', body);
+
+    var $ = cheerio.load(body);
+
+    var rodadas   = $('#competition-rounds').find('.tabela-jogos').eq(0).attr('class').split(" ");
+    var tabela    = "." + rodadas[2];
+
+    var rodada    = $(tabela).children('h3.stripe').text();
+    var data      = $(tabela).children('div.headline').eq(0).text();
+    var hora      = $(tabela).find('div.full-game-time').eq(0).text();
+    var mandante  = $(tabela).find('div.game-team-1').eq(0).text();
+    var visitante = $(tabela).find('div.game-team-2').eq(0).text();
+    var placar    = $(tabela).find('div.game-score').eq(0).text().trim().split("\n");
+
+    var gols_mandante  = placar[0].trim();
+    var gols_visitante = placar[2].trim();
+
+    var local = $(tabela).find('div.full-game-location span').eq(0).text().trim().split("\n");
+
+    var itens = 'Rodada: ' + rodada + '\n'
+                + 'Data: ' + data.trim() + '\n'
+                + 'Hora: ' + hora.trim() + '\n'
+                + 'Jogo: ' + mandante.trim() + ' [' + gols_mandante + ' X ' + gols_visitante + '] ' + visitante.trim() + '\n'
+                + 'Local: ' + local[3].trim();
+    console.log(itens);
+
 }
 
 request('http://www.cbf.com.br/competicoes/brasileiro-serie-a/tabela/2017#.WWe5jNPyuRt', cartola);
