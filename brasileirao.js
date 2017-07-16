6b0300dfba303468c5624f25a0eb4ff377353d1c
diedromeliga <diedromeliga@gmail.com>
2017-07-15 22:20:02 -0300

    pegando numero de rodadas e quantidade jogos em cada dia

--- brasileirao.js
+++ brasileirao.js
@@ -13,28 +13,32 @@ function cartola(error, response, body) {
     //console.log('body', body);
 
     var $ = cheerio.load(body);
-
     var rodadas   = $('#competition-rounds').find('.tabela-jogos').eq(0).attr('class').split(" ");
     var tabela    = "." + rodadas[2];
 
+    var count_rounds = $('#competition-rounds').find('.tabela-jogos').length
+    var count_rounds_day = $(tabela).children('div.headline').length;
+    var count_rounds_day_game = $(tabela).children('div.headline').siblings('.row').length();
+
+
     var rodada    = $(tabela).children('h3.stripe').text();
     var data      = $(tabela).children('div.headline').eq(0).text();
-    var hora      = $(tabela).find('div.full-game-time').eq(0).text();
-    var mandante  = $(tabela).find('div.game-team-1').eq(0).text();
-    var visitante = $(tabela).find('div.game-team-2').eq(0).text();
-    var placar    = $(tabela).find('div.game-score').eq(0).text().trim().split("\n");
+    var hora      = $(tabela).find('div.full-game-time').eq(1).text();
+    var mandante  = $(tabela).find('div.game-team-1').eq(1).text();
+    var visitante = $(tabela).find('div.game-team-2').eq(1).text();
+    var placar    = $(tabela).find('div.game-score').eq(1).text().trim().split("\n");
 
     var gols_mandante  = placar[0].trim();
     var gols_visitante = placar[2].trim();
 
-    var local = $(tabela).find('div.full-game-location span').eq(0).text().trim().split("\n");
+    var local = $(tabela).find('div.full-game-location span').eq(1).text().trim().split("\n");
 
     var itens = 'Rodada: ' + rodada + '\n'
                 + 'Data: ' + data.trim() + '\n'
                 + 'Hora: ' + hora.trim() + '\n'
                 + 'Jogo: ' + mandante.trim() + ' [' + gols_mandante + ' X ' + gols_visitante + '] ' + visitante.trim() + '\n'
                 + 'Local: ' + local[3].trim();
-    console.log(itens);
+    console.log(count_rounds_day_game);
 
 }
 
